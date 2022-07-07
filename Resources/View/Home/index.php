5943e4441744fcc4ebb8c45d455a9b67a39e55e1
Livington Lenin Lopez Cruz <livingtonllopezc124@gmail.com>
2022-07-07 09:30:28 -0500

    Add navbar lateral

--- Resources/View/Home/index.php
+++ Resources/View/Home/index.php
@@ -9,6 +9,7 @@ require "../Resources/View/Template/header.php";
 ?>
 
 
+
 <?php
 require "../Resources/View/Template/footer.php";
 
--- Resources/View/Template/header.php
+++ Resources/View/Template/header.php
@@ -6,9 +6,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/style.css">
     <title><?php echo CONFIG["APP_NAME"]?></title>
-    <link rel="shortcut icon" href="/images/svg/logo-svg.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
 </head>
 <body>
+
+<div style="display: flex;">
+<div style="width:20%;height:auto;background:blue">
+    
+    
+
+</div>
+<nav class="navbar">
+    
+    <div class="d-flex justify-content-center">
+        <nav id="buscador">
+            <div class="container-fluid">
+                <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+        <div>Micro</div>
+    </div>
+    <div class="d-flex justify-content-end">
+        <div>crear</div>
+        <div>Service</div>
+        <div>Notif</div>
+        <div>User</div>
+    </div>
+</nav>
+</div>
+
 
 
 </body>
